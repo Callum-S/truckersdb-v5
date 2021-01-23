7ac701ff386424685d574b7f723e47fc45e43458
Callum S <46674169+Callum-S@users.noreply.github.com>
2021-01-23 00:10:20 +0000

    Added Terms of Service

--- terms.php
+++ terms.php
@@ -9,23 +9,26 @@ require_once("./assets/common/head.php");
     <div class="container">
         <h2>Terms of Service</h2>
         <p>If you have any queries or concerns regarding anything on this page, please don't hesitate to <a href="./contact.php">contact us</a>.</p>
+        <h4>The Basics</h4>
+        <p>
+            By using the TruckersDB platform as a registered user, you agree to abide by the following terms and conditions. Our service is provided free of charge to all users. TruckersDB is not affiliated with SCS Software or TruckersMP in any way. If you require these terms in another format for any reason, please contact us and we will attempt to provide a suitable alternate format.
+        </p>
         <h4>User Restrictions</h4>
         <p>
-            Users must be at least thirteen (13) years of age in order to use the platform. Any user found to be under the age of 13 may have their account removed without notice. A valid, permanent email address is required to register with TruckersDB.
+            Users must be at least thirteen (13) years of age in order to use the platform. Any user found to be under the age of 13 may have their account removed without notice. A valid, permanent email address is required to register with TruckersDB. The platform and its users should be treated with respect at all times. We may remove a user's account with 7 days' notice if they are found to be abusing the platform or behaving in an inappropriate manner towards its users and staff.
         </p>
-        <h4>Heading</h4>
+        <h4>Reporting Vulnerabilities</h4>
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            If you find a security vulnerability anywhere within our platform, please don't hesitate to report it to us - do not attempt to exploit it. We will attempt to test the vulnerability in a safe environment, and provide a fix as soon as we can. To report a possible vulnerability, contact <a href="mailto:support@truckersdb.net"><strong>support@truckersdb.net</strong></a> and your message will be passed to a developer.
         </p>
-        <h5>Subheading</h5>
+        <h4>Inappropriate Content</h4>
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            TruckersDB regularly reviews user-submitted data such as images and text (for example a company description and gallery) to ensure that there is no inappropriate, unsuitable or illegal content being displayed. Some data may be reviewed by a third-party service at the time of submission in order to remove any inappropriate content. Users found to be displaying such content will have their accounts removed without notice, and the appropriate authorities will be notified if necessary. Forbidden content includes but is not limited to: harmful links or links to malware; adult or pornographic content, especially including minors; private personal information; racist, sexist, homophobic or similar text and images; and threats toward others.
         </p>
-        <ul>
-            <li><strong>Bullet Points</strong> Information goes here.</li>
-            <li><strong>Bullet Points</strong> Information goes here.</li>
-            <li><strong>Bullet Points</strong> Information goes here.</li>
-        </ul>
+        <hr>
+        <p>
+            These terms were last updated 23 Jan 2021. Although registered users will be notified of any major changes, we recommend reviewing these terms regularly.
+        </p>
     </div>
     
     <?php require_once("./assets/common/footer.php"); ?>
